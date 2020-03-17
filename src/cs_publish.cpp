d86804b3aa87fea13b395473ba39ccbe01f8b2d8
hi2ro5zzz <hi4ro9thu2nm5@gmail.com>
2020-03-17 11:56:19 +0900

    chance force func to sin or cos

--- src/cs_publish.cpp
+++ src/cs_publish.cpp
@@ -10,21 +10,30 @@ int main(int argc, char** argv)
     
     ros::Publisher cs_pub = nh.advertise<geometry_msgs::Wrench>("cs_msg",1);
 
-    ros::Rate loop_rate(100);
+    ros::Rate loop_rate(1000);
 
     geometry_msgs::Wrench force;
-
-    // プラグインへ力の情報を出力
-    force.force.x = 3;
-    force.force.y = 2;
-    force.force.z = 1;
+    int i = 0;
 
     while(ros::ok())
     {
+        // プラグインへ力の情報を出力
+        force.force.x = sin(i*3.14/60);
+        force.force.y = cos(i*3.14/60);
+        force.force.z = 1;
+
         cs_pub.publish(force);
+        i++;
+
         ros::spinOnce();
         loop_rate.sleep();
+
     }
+
+    force.force.x = 0;
+    force.force.y = 0;
+    force.force.z = 0;
+    cs_pub.publish(force);
 }
 
     
